db2592a9a940ca67eb5061308c76f6d6be716ed2
Somchay <Somchayk@yahoo.com>
2019-12-23 06:47:48 -0700

    finished the api controller and set up static page

---
+++ app/controllers/api/somchay_portfolios_controller.rb
@@ -0,0 +1,39 @@
+class Api::SomchayPortfoliosController < ApplicationController
+
+def index
+  render json: Blog.all
+end
+
+def create
+  @blog = Blog.new(blog_params)
+  if @blog.save
+    render json: @blog  
+  else
+    render json: { errors: @blog.errors }, status: :unprocessable_entity
+  end
+end
+
+def update
+  @blog = Blog.find(params[:id])
+  if @blog.update(blog_params)
+    render json: @blog
+  else
+    render json: { errors: @blog.errors }, status: :unprocessable_entity
+  end
+end
+
+def destroy 
+  @blog = Blog.find(params[:id]).destroy
+  render json: { message: 'Item Deleted'}
+end
+
+private
+def blog_params
+  params.require(:blog).permit( :title, :body)
+end
+
+
+
+
+
+end
---
+++ test/controllers/api/somchay_portfolios_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Api::SomchayPortfoliosControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
